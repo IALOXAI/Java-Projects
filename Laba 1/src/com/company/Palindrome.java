6e9530520dce1a58351f2efcd56f2d8d021738a0
IALOXAI <71461482+IALOXAI@users.noreply.github.com>
2020-12-03 00:21:45 +0300

    Update Palindrome.java

--- Laba 1/src/com/company/Palindrome.java
+++ Laba 1/src/com/company/Palindrome.java
@@ -8,7 +8,7 @@ public class Palindrome {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите строку для ее инвертирования и проверки на палиндромность");
         String input_string = scan.nextLine();
-        System.out.println(Reverse_String(input_string));
+        System.out.println("Инвертированная строка:\n" + Reverse_String(input_string));
         System.out.println(Is_Palindrome(input_string));
         scan.close();
     }
@@ -19,10 +19,8 @@ public class Palindrome {
         for (int pos = s.length() - 1; pos > -1; pos--) {
             finalString.append(s.charAt(pos));
         }
-        System.out.println("Инвертированная строка:/n");
-        return "finalString.toString();
+        return finalString.toString();
     }
-
     public static String Is_Palindrome(String s) {
         // Проверка на палиндромность
         String reverse = Reverse_String(s);
